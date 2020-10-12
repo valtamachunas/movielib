f4990cbe223b70f0ee3ee548e0fbd39b0f3b3afe
JoseP-UR <urivesrosa@gmail.com>
2020-10-12 17:20:25 -0300

    header

--- src/components/Header/index.js
+++ src/components/Header/index.js
@@ -4,14 +4,16 @@ import './header.css';
 
 class Header extends React.Component {
     render() {
-      return (
-          <header className="header-container">
-              <img className='logo'src={logo} />
-              <a className='home'href="">teste</a>
-
-          </header>
-      );
+        return (
+            <header className="header-container">
+                <div className="logo-container">
+                    <img className="logo" src={logo} />
+                    <h1 className="title">MovieLib</h1>
+                </div>
+                <a className='home-button' href="">teste</a>
+            </header>
+        );
     }
-  }
+}
 
 export default Header;
