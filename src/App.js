d58fa8b18e5060e904a54365ea3726cc2baf8e90
JoseP-UR <urivesrosa@gmail.com>
2020-10-17 16:16:23 -0300

    changed from create-react-app to barebones react

--- src/App.js
+++ src/App.js
@@ -1,16 +1,16 @@
-import React from 'react';
-import './App.css';
-import Header from './components/Header'
+import React, { Component} from "react";
+import {hot} from "react-hot-loader";
+import "./App.sass";
 
-class App extends React.Component {
-  render() {
-    return (
-      <div>
-        <Header />
-        <h1>Hello World</h1>
+class App extends Component{
+  render(){
+    return(
+      <div className="App">
+        <header className="header">this is the header</header>
+        <div className="main"> this is the main area</div>
       </div>
-      );
+    );
   }
 }
 
-export default App;
+export default hot(module)(App);
